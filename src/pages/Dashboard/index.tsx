681f4599b374d8b1dc40c1ebce23238c1bd03dcb
BrHCastro <hendecastro@gmail.com>
2022-01-09 22:01:29 -0300

    Consumo da api do github com axios Feito requisição para api e validação do input no dashboard

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,33 +1,75 @@
 import React from "react";
 
-import { Title, Form, Repos } from "./styles";
+import { api } from "../../services/api";
+import { Title, Form, Repos, Error } from "./styles";
 import Logo from "../../assets/logo.svg";
 import { FiChevronRight } from "react-icons/fi";
 
+interface GithubRepository {
+  full_name: string;
+  description: string;
+  owner: {
+    login: string;
+    avatar_url: string;
+  };
+}
+
 export const Dashboard: React.FC = () => {
+  const [repos, setRepos] = React.useState<GithubRepository[]>([]);
+  const [newRepo, setNewRepo] = React.useState("");
+  const [inputError, setInputError] = React.useState("");
+
+  function handleInputChange(event: React.ChangeEvent<HTMLInputElement>): void {
+    setNewRepo(event.target.value);
+  }
+
+  async function handleAddRepo(
+    event: React.FormEvent<HTMLFormElement>
+  ): Promise<void> {
+    event.preventDefault();
+
+    if (!newRepo) {
+      setInputError("Digite o usuário/nome do repositório");
+      return;
+    }
+
+    const response = await api.get<GithubRepository>(`repos/${newRepo}`);
+    const repository = response.data;
+
+    setRepos([...repos, repository]);
+    setNewRepo("");
+    setInputError("");
+  }
+
   return (
     <>
       <img src={Logo} alt="Logo" />
       <Title>Catálogo de Repositórios do GitHub</Title>
-      <Form>
-        <input type="text" placeholder="UserName/Repository" />
+      <Form onSubmit={handleAddRepo} hasError={Boolean(inputError)}>
+        <input
+          type="text"
+          placeholder="UserName/Repository"
+          onChange={handleInputChange}
+        />
         <button type="submit">Buscar</button>
       </Form>
+
+      {inputError && <Error>{inputError}</Error>}
+
       <Repos>
-        <a href="/repositories">
-          <img
-            src="https://avatars.githubusercontent.com/u/66978333?v=4"
-            alt="Imagem Repositorio"
-          />
-          <div>
-            <strong>brhcastro/repo-exemplo</strong>
-            <p>
-              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quo
-              veniam odio minus id.
-            </p>
-          </div>
-          <FiChevronRight size={20} />
-        </a>
+        {repos.map((repository) => (
+          <a href="/repositories" key={repository.full_name}>
+            <img
+              src={repository.owner.avatar_url}
+              alt={repository.owner.login}
+            />
+            <div>
+              <strong>{repository.full_name}</strong>
+              <p>{repository.description}</p>
+            </div>
+            <FiChevronRight size={20} />
+          </a>
+        ))}
       </Repos>
     </>
   );
