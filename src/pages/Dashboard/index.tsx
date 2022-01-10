32461eccfd61681c5bf874bef5e6186732236df3
BrHCastro <hendecastro@gmail.com>
2022-01-09 21:11:37 -0300

    Estilização inicial página dashboard

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,7 +1,34 @@
 import React from "react";
 
-import { Title } from "./styles";
+import { Title, Form, Repos } from "./styles";
+import Logo from "../../assets/logo.svg";
+import { FiChevronRight } from "react-icons/fi";
 
 export const Dashboard: React.FC = () => {
-  return <Title>Dashboard</Title>;
+  return (
+    <>
+      <img src={Logo} alt="Logo" />
+      <Title>Catálogo de Repositórios do GitHub</Title>
+      <Form>
+        <input type="text" placeholder="UserName/Repository" />
+        <button type="submit">Buscar</button>
+      </Form>
+      <Repos>
+        <a href="/repositories">
+          <img
+            src="https://avatars.githubusercontent.com/u/66978333?v=4"
+            alt="Imagem Repositorio"
+          />
+          <div>
+            <strong>brhcastro/repo-exemplo</strong>
+            <p>
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quo
+              veniam odio minus id.
+            </p>
+          </div>
+          <FiChevronRight size={20} />
+        </a>
+      </Repos>
+    </>
+  );
 };
