47b054441fed36e738862f3d59a69c3ea7369b60
BrHCastro <hendecastro@gmail.com>
2022-01-10 23:01:52 -0300

    Utilização do React.lazy

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,13 +1,18 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
-import { Dashboard } from "../pages/Dashboard";
-import { Repo } from "../pages/Repo";
+// import { Dashboard } from "../pages/Dashboard";
+// import { Repo } from "../pages/Repo";
+
+const Dashboard = React.lazy(() => import("../pages/Dashboard"));
+const Repo = React.lazy(() => import("../pages/Repo"));
 
 export const Routes: React.FC = () => {
   return (
-    <Switch>
-      <Route path="/" exact component={Dashboard} />
-      <Route path="/repositories/:repository+" component={Repo} />
-    </Switch>
+    <React.Suspense fallback={"Loading..."}>
+      <Switch>
+        <Route path="/" exact component={Dashboard} />
+        <Route path="/repositories/:repository+" component={Repo} />
+      </Switch>
+    </React.Suspense>
   );
 };
